c967deff14e9f4cdcf205b010d0ec1d53c760c80
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-08-08 22:19:44 +0530

    8th aug assignment movie and lodash

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,90 +1,102 @@
-const express = require('express');
-const abc = require('../introduction/intro')
+const express = require("express");
 const router = express.Router();
-const lodash= require("lodash")
-const logger = require("../logger/logger.js");
-const assignment = require("../util/helper.js");
-const formatter = require("../validator/formatter.js");
-// <<<<<<< Updated upstream
+const lodash = require("lodash");
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+// -------------------------- 8th aug assignment part 1-----------------------------//
 
-   
-    res.send('My second ever api!')})
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let result = lodash.chunk(months, [(size = 4)]);
+console.log("lodash example is", result);
 
-router.get("/test-me", function (req, res) {
-  console.log("My batch is", abc.name);
+let oddno = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+let result2 = lodash.tail(oddno);
+console.log("tail example is", result2);
 
-  // 8th aug assignment
+let arrays = [1, 2, 3];
+let array2 = [4, 5, 6];
+let array3 = [7, 8];
+let array4 = [9, 10, 11];
+let array5 = [12, 13, 14, 15];
+let result3 = lodash.union(arrays, array2, array3, array4, array5);
+console.log("union example is", result3);
 
-  let months =['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 
-  'October', 'November', 'December'];
-  let result=lodash.chunk(months, [size=4]);
-  console.log("lodash example is" ,result);
+let pairs = [
+  ["horror", "The Shining"],
+  ["drama", "Titanic"],
+  ["thriller", "Shutter Island"],
+  ["fantasy", "Pans Labyrinth"],
+];
+let result4 = lodash.fromPairs(pairs);
+console.log("from paris example is", result4);
 
+// --------------------------- assignment 8 aug part 2-----------------------------//
 
-  let oddno=[1,3,5,7,9,11,13,15,17,19]
-  let result2=lodash.tail(oddno)
-  console.log("tail example is",result2)
+let movie = [
+  {
+    id: 1,
+    name: "The Shining",
+  },
+  {
+    id: 2,
+    name: "Incendies",
+  },
+  {
+    id: 3,
+    name: "Rang de Basanti",
+  },
+  {
+    id: 4,
+    name: "Finding Nemo",
+  },
+];
+let movie2 = [
+  "Rang de basanti",
+  "The shining",
+  "Lord of the rings",
+  "Batman begin",
+];
 
-
-  let arrays= [1,2,3];
-  let array2=[4,5,6];
-  let array3=[7,8];
-  let array4=[9,10,11]
-  let array5=[12,13,14,15]
-   let result3=lodash.union(arrays,array2,array3,array4,array5);
-  console.log("union example is",result3)
-
-
-  let pairs= [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]];
-   let result4=lodash.fromPairs(pairs)
-   console.log("from paris example is",result4)
-
-  abc.printName();
-
-  logger.logger();
-
-  assignment.assignment();
-
-  formatter.myformatter();
-
-  res.send("My second ever api!");
+// question - 1st
+router.get("/movies", function (req, res) {
+  res.send(movie2);
 });
 
-
-router.get("/test-you", function (req, res) {
-  res.send("This is the second routes implementation");
+// question - 2nd and 3rd
+router.get("/movies/:index", function (req, res) {
+  let index = req.params.index;
+  if (index > movie2.length - 1) {
+    return res.send("plzz provide a valid index");
+  }
+  return res.send(movie2[index]);
 });
-// >>>>>>> Stashed changes
 
+// question - 4th
+router.get("/films", function (req, res) {
+  res.send(movie);
+});
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
+// question - 5th
+router.get("/films/:filmId", function (req, res) {
+  let filmid = req.params.filmId;
+  for (let i = 0; i < movie.length; i++) {
+    if (movie[i].id == filmid) {
+      return res.send(movie[i]);
+    }
+  }
+  return res.send(" No movie exists with this id");
+});
 
 module.exports = router;
