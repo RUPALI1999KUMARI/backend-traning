b945bdf4e4f3893ceb33391aa8a37fb0bcc3c9cd
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-08-13 20:36:50 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+
+const route = require('./routes/route.js');
+const mongoose =require('mongoose')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://RUPALIKUMARI:Ja8ibOldIbkNORKK@cluster0.wwhf9wj.mongodb.net/?retryWrites=true&w=majority"
+,{
+    useNewUrlParser: true,
+}
+).then( ()=> {console.log( "MongoDb is connected" )} )
+.catch(err => console.log(err))
+
+app.use('/', route);
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
