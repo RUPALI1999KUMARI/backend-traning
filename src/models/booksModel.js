90bf81a8019b935ba9dc2954daa32625ab71378c
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-08-18 23:22:05 +0530

    17th aug assignment complited

---
+++ src/models/booksModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    bookName: { type: String },
+    authorId: { type: Number, required: true },
+    prices: { type: Number },
+    rating: { type: Number },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Book1", bookSchema);
